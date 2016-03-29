429e4112cce6ffda2fecccbe4e4c04ba9785af46
AJFiori <AJFioriiii@email.neit.edu>
2016-03-28 22:26:07 -0400

    working on form

--- PhpProject1/week1/lab/index.php
+++ PhpProject1/week1/lab/index.php
@@ -1,17 +1,49 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     </head>
     <body>
         <?php
         // put your code here
+        
+        require_once './functions/dbconnect.php';
+        require_once './functions/util.php';
+        
+        
+        $address = filter_input(INPUT_POST, 'address');
+        
+        
+        $address = getAllAddress();
+        
+        if ( isPostRequest() ) {
+            
+            
+            if ( empty($address) ) {
+                $message = 'Sorry address is Empty';
+            } 
+             else if ( addPhone($address) ) {
+                $message = 'Address Added';
+                $address = '';
+                
+            }
+            
+            
+        }
+                
+                
+        include './templates/messages.html.php';
+        include './templates/AddAddress-form.html.php';
+        
+        include './templates/View-Address.html.php';
         ?>
+        
+        
+        
     </body>
 </html>
