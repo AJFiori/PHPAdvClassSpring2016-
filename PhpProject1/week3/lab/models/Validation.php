dc0f4b5ef280a171504790d199d64c961ea6f24d
AJFiori <AJFioriiii@email.neit.edu>
2016-04-25 18:49:23 -0400

    finished

--- PhpProject1/week3/lab/models/Validation.php
+++ PhpProject1/week3/lab/models/Validation.php
@@ -5,19 +5,19 @@
 */
 class Validation
 {
-    public function emailIsValid($tmp)
+    public function emailIsValid($eCheck)
     {
-    	return(preg_match('/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/', $tmp));
+    	return(preg_match('/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/', $eCheck));
     }
 
-    public function valueIsValid($tmp)
+    public function valueIsValid($eCheck)
     {
-    	return(preg_match('/^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/', $tmp));
+    	return(preg_match('/^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/', $eCheck));
     }
 
-    public function valueIsEmpty($tmp)
+    public function valueIsEmpty($eCheck)
     {
-    	return(empty($tmp));
+    	return(empty($eCheck));
     }
 }
 
